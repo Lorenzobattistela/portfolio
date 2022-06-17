130e9751c6e3f0006fa5c63320bf0e1186cf6b2c
Lorenzobattistela <lorenzowottrich@gmail.com>
2022-06-17 08:38:08 -0300

    contact page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,29 +1,72 @@
+import { faCss3, faGitAlt, faHtml5, faPython, faReact } from "@fortawesome/free-brands-svg-icons"
+import { faFlask } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useState, useEffect } from "react"
+import Loader from "react-loaders"
 import AnimatedLetters from "../AnimatedLetters"
 import "./index.scss"
 
  const About = () => {
+
+    const [letterClass, setLetterClass] = useState("text-animate")
+    useEffect(()=> {
+        setTimeout(()=> {
+            return setLetterClass("text-animate-hover")
+        }, 3000)
+    }, [])
     return(
-        <div className="container about-page">
-            <div className="text-zone">
-                <h1>
-                    <AnimatedLetters strArray={["A","b","o","u","t"," ", "m", "e"]} idx={15}/>
-                </h1>
-                <p>
-                    I'm very ambitious front-end developer looking for a role in
-                    established IT company with the opportunity to work with the latest
-                    technologies on challenging and diverse projects.
-                </p>
-                <p align="LEFT">
-                    I'm quietly confident, naturally curious, and perpetually working on
-                    improving my chops one design problem at a time.
-                </p>
-                <p>
-                    If I need to define myself in one sentence that would be a family
-                    person, father of a beautiful daughter, a sports fanatic,
-                    photography enthusiast, and tech-obsessed!!!
-                </p>
+        <>
+            <div className="container about-page">
+                <div className="text-zone">
+                    <h1>
+                        <AnimatedLetters letterClass={letterClass} strArray={["A","b","o","u","t"," ", "m", "e"]} idx={15}/>
+                    </h1>
+                    <p>
+                        I'm very ambitious front-end developer looking for a role in
+                        established IT company with the opportunity to work with the latest
+                        technologies on challenging and diverse projects.
+                    </p>
+                    <p align="LEFT">
+                        I'm quietly confident, naturally curious, and perpetually working on
+                        improving my chops one design problem at a time.
+                    </p>
+                    <p>
+                        If I need to define myself in one sentence that would be a family
+                        person, father of a beautiful daughter, a sports fanatic,
+                        photography enthusiast, and tech-obsessed!!!
+                    </p>
+                </div>
+
+                <div className="stage-cube-cont">
+                    <div className="cubespinner">
+                        <div className="face1">
+                            <FontAwesomeIcon icon={faReact} color="#dd0031"/>
+                        </div>
+
+                        <div className="face2">
+                            <FontAwesomeIcon icon={faHtml5} color="#f06529"/>
+                        </div>
+
+                        <div className="face3">
+                            <FontAwesomeIcon icon={faGitAlt} color="#28a4d9"/>
+                        </div>
+
+                        <div className="face4">
+                            <FontAwesomeIcon icon={faCss3} color="#5ed4f4"/>
+                        </div>
+
+                        <div className="face5">
+                            <FontAwesomeIcon icon={faPython} color="#efd81d"/>
+                        </div>
+
+                        <div className="face6">
+                            <FontAwesomeIcon icon={faFlask} color="#ec4d28"/>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+            <Loader type="pacman"/>
+        </>
     )
  }
 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import AnimatedLetters from "../AnimatedLetters";
 import LogoTitle from '../../assets/images/logo-s.png'
 import "./index.scss";
 import Logo from "./Logo";
+import Loader from "react-loaders";
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -15,44 +16,43 @@ const Home = () => {
             return setLetterClass("text-animate-hover")
         }, 4000)
     }, [])
-    // useEffect(() => {
-    //     return setTimeout(() => {
-    //       setLetterClass('text-animate-hover')
-    //     }, 4000)
-    //   }, [])    
+      
 
     return(
-        <div className="containter home-page">
-            <div className="text-zone">
-            <h1>
-            <span className={letterClass}>H</span>
-            <span className={`${letterClass} _12`}>i,</span>
-            <br />
-            <span className={`${letterClass} _13`}>I</span>
-            <span className={`${letterClass} _14`}>'m</span>
-            <img
-              src={LogoTitle}
-              alt="JavaScript Developer Name, Web Developer Name"
-            />
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={nameArray}
-              idx={15}
-            />
-            <br />
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={jobArray}
-              idx={22}
-            />
-          </h1>
-          <h2>Front End Developer / JavaScript Expert / Youtuber</h2>
-          <Link to="/contact" className="flat-button">
-            CONTACT ME
-          </Link>
+        <>
+          <div className="containter home-page">
+              <div className="text-zone">
+              <h1>
+              <span className={letterClass}>H</span>
+              <span className={`${letterClass} _12`}>i,</span>
+              <br />
+              <span className={`${letterClass} _13`}>I</span>
+              <span className={`${letterClass} _14`}>'m</span>
+              <img
+                src={LogoTitle}
+                alt="JavaScript Developer Name, Web Developer Name"
+              />
+              <AnimatedLetters
+                letterClass={letterClass}
+                strArray={nameArray}
+                idx={15}
+              />
+              <br />
+              <AnimatedLetters
+                letterClass={letterClass}
+                strArray={jobArray}
+                idx={22}
+              />
+            </h1>
+            <h2>Front End Developer / JavaScript Expert / Youtuber</h2>
+            <Link to="/contact" className="flat-button">
+              CONTACT ME
+            </Link>
+          </div>
+          <Logo/>
         </div>
-        <Logo/>
-      </div>
+        <Loader type="pacman"/>
+      </>
     )
 }
 
